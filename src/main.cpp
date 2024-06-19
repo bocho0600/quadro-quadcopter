2f51b9790ec4350b0671afa87689c4ee3c39571f
unknown <nguyenleminh1002@gmail.com>
2024-06-19 13:02:17 +1000

    full code for MPU9250 configuration

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,43 @@
-// Last update: 2021-07-29 20:00:00
+/* A ByPo Quadcopter Robot project of Kelvin Le
+Contact: 
+- Email: nguyenleminh1002@gmail.com
+         minhnguyen.le@qut.edu.au
+- Linkedin: https://www.linkedin.com/in/kelvin-le-1002/   */
+
+
 #include <Wire.h>
 #include <Arduino.h>
 #include <stdint.h>
 #include "MPUinit.h"
 
-
 void setup()
 {
+
   Serial.begin(115200);
   Wire.begin();
   Wire.setClock(400000); // Set the I2C clock to 400kHz
   delay(250);            // Wait for the MPU9250 to power up
-  PowerManagementMPU(); // Wake up the MPU9250
+  PowerManagementMPU();  // Wake up the MPU9250
+  MPUCalibration();      // Calibrate the gyroscope, let the ByPo stable in the first 2 seconds to measure the average value
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
-  GyroConfiguration(); // Configure the gyroscope
-  Serial.print("Roll: ");   // x-axis
+  GyroConfiguration();    // Configure the gyroscope
+  RateCorrection();       // Correct the gyroscope data by calibration data
+  Serial.print("Roll: "); // x-axis
   Serial.print(RateRoll);
   Serial.print(" Pitch: "); // y-axis
   Serial.print(RatePitch);
-  Serial.print(" Yaw: ");  // z-axis
+  Serial.print(" Yaw: "); // z-axis
   Serial.println(RateYaw);
+  if ((RatePitch < -2) || (RatePitch > 2) || (RateRoll < -2) || (RateRoll > 2) || (RateYaw < -2) || (RateYaw > 2))
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 }
-
